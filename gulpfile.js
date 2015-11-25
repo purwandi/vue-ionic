0b6ab07f3900faf666377c0062dc8ab22b34084b
Purwandi <free6300@gmail.com>
2015-11-25 09:53:10 +0700

    Import ionicframework js.

--- gulpfile.js
+++ gulpfile.js
@@ -17,4 +17,35 @@ elixir(function(mix) {
     mix
         .sass('ionic.scss');
 
+    mix
+        .scripts([
+            // Base
+            'ionic.js',
+
+            // Utils
+            'utils/delegateService.js',
+            'utils/dom.js',
+            'utils/events.js',
+            'utils/gestures.js',
+            'utils/platform.js',
+            'utils/poly.js',
+            'utils/tap.js',
+            'utils/activator.js',
+            'utils/utils.js',
+            // 'utils/list.js',
+            'utils/keyboard.js',
+            'utils/viewport.js',
+
+                    // Views
+            'views/view.js',
+            'views/scrollView.js',
+            'views/scrollViewNative.js',
+            'views/listView.js',
+            'views/modalView.js',
+            'views/sideMenuView.js',
+            'views/sliderView.js',
+            'views/toggleView.js'
+
+        ], 'public/js/ionic.js');
+
 });
